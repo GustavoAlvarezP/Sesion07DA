3d76e10c161fc41661db66dd62226545647e1a90
GustavoAlvarezP <g.alvarez0202@gmail.com>
2021-10-13 22:05:11 -0500

    Experiencia de practica 2: Acceso a la base de datos

---
+++ AppMVC01/Global.asax.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace AppMVC01.Controllers
+{
+    [Authorize]
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+        public class HomeController: Controller { 
+        public ActionResult index()
+        {
+            return View();
+        }
+    }
+    }
+}
